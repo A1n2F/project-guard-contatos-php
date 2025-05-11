f0a99171c45e457847bc2e33c4575327016162d3
A1n2F <alissonfferreira22@gmail.com>
2025-05-11 12:00:01 -0300

    Layout tela Home

--- views/index.view.php
+++ views/index.view.php
@@ -1,1 +1,19 @@
-oi
+<div class="flex flex-col items-center max-w-screen-lg h-screen justify-center">
+    <div>
+        <div class="text-gray-200">
+            <p class="py-2 text-lg">Bem vindo ao</p>
+            <h1 class="text-6xl font-bold">Guard-Contatos</h1>
+            <p class="mt-3">Guarde seus <span class="italic font-semibold">contatos</span> com segurança</p>
+
+            <div class="flex gap-4 mt-4">
+                <a href="/login" class="text-lg text-lime-500 font-bold hover:text-lime-600 transition-colors cursor-pointer">
+                    Login
+                </a>
+
+                <a href="/registrar" class="text-lg text-lime-500 font-bold hover:text-lime-600 transition-colors cursor-pointer">
+                    Registrar
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
--- views/template/app.php
+++ views/template/app.php
@@ -7,43 +7,9 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body class="bg-stone-950 text-stone-300">
-    <header class="bg-stone-900">
-        <nav class="mx-auto max-w-screen-xl flex justify-between py-4">
-            <div class="font-bold text-xl tracking-wide">
-                Book Wise
-            </div>
-
-            <ul class="flex gap-4 font-bold">
-                <li><a href="/" class="text-lime-500">Explorar</a></li>
-
-                <?php if(auth()): ?>
-                    <li><a href="/meus-livros" class="hover:underline">Meus Livros</a></li>
-                <?php endif; ?>
-            </ul>
-
-            <ul>
-                <?php if(auth()): ?>
-                    <div class="flex gap-2">
-                        <li><span>Oi, <?=auth()->nome?></span></li>
-                        <li><a href="/logout" class="hover:underline">Sair</a></li>
-                    </div>
-                <?php else: ?>
-                    <li><a href="/login" class="hover:underline">Fazer login</a></li>
-                <?php endif; ?>
-            </ul>
-        </nav>
-    </header>
-
+<body class="bg-black">
     
-    <main class="mx-auto max-w-screen-xl">
-        <?php if($mensagem = flash()->get('mensagem')): ?>
-            <div class="border-green-800 bg-green-900 text-green-400 font-bold px-4 py-2 rounded-md text-center">
-                <?=$mensagem?>
-            </div>
-        <?php endif; ?>
+    <?php require "../views/{$view}.view.php"; ?>
 
-        <?php require "../views/{$view}.view.php"; ?>
-    </main>
 </body>
 </html>
